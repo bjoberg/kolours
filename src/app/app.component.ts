b386399d82e4735bc7eb2d13195b16ef36800f9b
Brett Oberg <brett@obergmail.com>
2016-10-18 00:04:24 +0800

    Background changes when user selects button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,8 @@ export class RGB {
 })
 
 export class AppComponent {
+  // @HostBinding('style.background-color') 
+
   title = 'Kolours';
   
   // Starting background color
@@ -28,15 +30,16 @@ export class AppComponent {
   /**
    * Fired when the user clicks the #update-background button. This function will read the current input values and set the background color to these values.
    * 
-   * @param rgb: RGB object bound to the input fields. Holds the rbg(,,) values for the new background  
+   * @param rgb: RGB object bound to the input fields. Holds the rgb(,,) values for the new background  
    */
   updateBackgroundColor(rgb) {
     // 1. Make sure all of the inputs are in the correct form
-
+    
     // 2. Construct the new background color
-    var updatedBackgroundColor = "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ");";
+    var updatedBackgroundColor = "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ")";
 
-    // 3. Set the background of the <body> to the new background color
+    // 3. Set the background of the <body> to the new background color. Right now I am using direct DOM manipulation because I could not find a way to access the <body> via typescript. 
+    document.body.style.background = updatedBackgroundColor;
       // 3.1 In the future this is when the animation should happen 
   }
 }
